ab1e50140a3ee5951e7e0a00b8d3df5a90cd0a69
Nicolas Salas V <nikosalas@gmail.com>
2015-11-27 01:03:54 -0300

    Empezar ABB

--- src/abb.c
+++ src/abb.c
@@ -1,1 +1,27 @@
 #include "abb.h"
+
+
+void abb_new(abb *a){
+  a->arbol = (struct abb_nodo *)malloc(sizeof(struct abb_nodo));
+  a->arbol = NULL;
+}
+
+
+
+
+
+
+
+
+static void _free_abb(struct abb_nodo *nodo){
+  if(nodo == NULL) return;
+
+  _free_abb(nodo->izq);
+  _free_abb(nodo->der);
+
+  if(nodo->izq != NULL) free(nodo->izq);
+  if(nodo->der != NULL) free(nodo->der);
+
+  free_entry(nodo->e);
+  free(e);
+}
--- src/abb.h
+++ src/abb.h
@@ -1,4 +1,24 @@
 #ifndef TAREA3_ALGORITMOS_ABB_H
 #define TAREA3_ALGORITMOS_ABB_H
 
+#include "entry.h"
+
+typedef struct {
+  struct abb_nodo *arbol;
+} abb;
+
+struct abb_nodo {
+  entry *e;
+  struct abb_nodo *izq;
+  struct abb_nodo *der;
+};
+
+void abb_new(abb *a);
+void abb_insertar(abb *a, entry *e);
+void* abb_buscar(abb *a, const char *key);
+void abb_eliminar(abb *a, const char *key);
+void abb_dispose(abb *a);
+
+
+
 #endif //TAREA3_ALGORITMOS_ABB_H
