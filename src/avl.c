648ff92768d05258fa550d99630b5e7ff6555ca5
Nicolas Salas V <nikosalas@gmail.com>
2015-12-05 04:14:23 -0300

    Iniciar AVL => New y Búsqueda

--- src/avl.c
+++ src/avl.c
@@ -1,1 +1,33 @@
+#include <string.h>
+#include <stdlib.h>
+
 #include "avl.h"
+
+
+void avl_new(avl *a){
+  a->raiz = NULL;
+}
+
+
+void avl_insertar(avl* a, const char *key, void *val, int valsize) {
+  return;
+}
+
+
+static void *_avl_buscar(struct avl_nodo *nodo, const char *key){
+  if(nodo == NULL) return NULL;
+
+  int cmp = strcmp(key, nodo->e->key);
+
+  if(cmp < 0) return _avl_buscar(nodo->izq, key); // por la izquierda
+  else if (cmp > 0) return _avl_buscar(nodo->der, key); // por la derecha
+
+  // cmp == 0;
+  return nodo->e->val;
+}
+
+void *avl_buscar(avl* a, const char *key) {
+  return _avl_buscar(a->raiz, key);
+}
+
+
--- src/avl.h
+++ src/avl.h
@@ -1,4 +1,24 @@
 #ifndef TAREA3_ALGORITMOS_AVL_H
 #define TAREA3_ALGORITMOS_AVL_H
 
+#include "entry.h"
+
+typedef struct {
+  struct avl_nodo *raiz;
+} avl;
+
+struct avl_nodo
+{
+  entry *e;
+  struct avl_nodo *izq;
+  struct avl_nodo *der;
+  int altura;
+};
+
+void avl_new(avl *a);
+void avl_insertar(avl *a, const char *key, void *val, int valsize);
+void* avl_buscar(avl *a, const char *key);
+void avl_eliminar(avl *a, const char *key);
+void avl_dispose(avl *a);
+
 #endif //TAREA3_ALGORITMOS_AVL_H
