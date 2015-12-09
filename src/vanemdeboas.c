e3f7900775faf8ed93512264b5994c5ad89fcb3e
Nicolas Salas V <nikosalas@gmail.com>
2015-12-09 02:39:51 -0300

    Generar esqueleto de Van Emde Boas

--- src/vanemdeboas.c
+++ src/vanemdeboas.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "vanemdeboas.h"
+
+// sólo aceptamos strings de largo 15
+static unsigned int _hashstring(const char *key){
+  if(strlen(key)>0){
+    unsigned int n;
+    switch(key[0]){
+      case 'G':
+        n=0;
+        break;
+      case 'C':
+        n=1;
+        break;
+      case 'A':
+        n=2;
+        break;
+      case 'T':
+        n=3;
+        break;
+    }
+    n=n<<(2*strlen(key+1));
+    return n + _hashstring(key+1);
+  }
+  else
+    return 0;
+}
+
+// retorna los k bits más significativos de x
+static unsigned int _highbits(unsigned int x, unsigned int k){
+  return x>>k;
+}
+
+static unsigned int _lowbits(unsigned int x, unsigned int k){
+  return 0; // pendiente
+}
--- src/vanemdeboas.h
+++ src/vanemdeboas.h
@@ -1,4 +1,29 @@
 #ifndef TAREA3_ALGORITMOS_VANEMDEBOAS_H
 #define TAREA3_ALGORITMOS_VANEMDEBOAS_H
 
+#include "entry.h"
+
+// Fuentes para entender el source
+// (1): http://www.daimi.au.dk/~gudmund/dynamicF04/vEB.pdf
+// (2): Apuntes del profe
+
+typedef struct {
+  struct vebtree *veb;
+} vanemdeboas;
+
+struct vebtree {
+  unsigned int size;
+  //int max;
+  //int min;
+  struct vebtree *top;
+  struct vebtree **min;
+};
+
+void vanemdeboas_new(vanemdeboas* veb);
+void vanemdeboas_insertar(vanemdeboas* veb);
+void vanemdeboas_buscar(vanemdeboas *veb, const char *key);
+void vanemdeboas_eliminar(vanemdeboas *veb, const char *key);
+void vanemdeboas_dispose(vanemdeboas *veb);
+
+
 #endif //TAREA3_ALGORITMOS_VANEMDEBOAS_H
